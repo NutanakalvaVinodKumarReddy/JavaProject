aca91dca7628e968db332efa237e1458a0b43f16
Vinod Reddy <nutanakalvavinodkumar@gmail.com>
2022-09-08 15:14:42 +0530

    08-09-2022 ->OOPS Concepts learnt

--- src/com/rxcorp/java/learning/Arrays.java
+++ src/com/rxcorp/java/learning/Arrays.java
@@ -1,27 +1,54 @@
 package com.rxcorp.java.learning;
 /*
-Array is a collection of similar data type of element which has continous memory location.
+Array is an indexed collection of fixed number of homogeneous data type of element which has continuous memory location.
 We can store a fixed set of elements in an Array.
 These are Java-Index based.
+To declare n number of values then we go for Arrays.
+Each element in Array is identified with index.
+Every Array is an Object. Hence we can create Arrays by using "new" keyword.
+
+**Advantages:
+1. Store n number of values with a single variable name.
+2. Readability of the code will be improved.
+
+**Dis-advantage:
+1. Fixed size of elements
+2. Stores only Homogenous DataTypes
+
+Types of Array Declaration:
+1. 1-D Array
+2. 2-D Array
+
+TODO: https://www.youtube.com/watch?v=HSp0UX9Z8Zw&ab_channel=DurgaSoftwareSolutions -->Part1
+
  */
 public class Arrays {
     public static void main(String[] args){
-        int arr1[] = new int[5];
-        arr1[0] =1001;
-        arr1[1] =1005;
-        arr1[2] =1002;
-        arr1[3] =1003;
-        arr1[4] =1004;
-        for(int i = 0; i<=arr1.length -1;i++){
-            System.out.println(arr1[i]);
-        }
+        int[] a; //This is recommended as the name is clearly separated from type
+                 //We should not delcare the size of an array during declaration.
+        int []b;
+        int c[];
 
-        String arr2[] = {"Apple","Banana","Orange"};
+        int[] names = new int[6];
+        System.out.println(names.getClass().getName()); //[I
 
-        for(int i = 0; i<=arr2.length-1;i++){
-            System.out.println(arr2[i]);
-        }
+        short[] a1 = new short[6];
+        System.out.println(a1.getClass().getName()); //[S
 
+        byte[] a2 = new byte[6];
+        System.out.println(a2.getClass().getName()); //[B
 
+        double[] a3 = new double[6];
+        System.out.println(a3.getClass().getName()); //[D
+
+        boolean[] a4 = new boolean[6];
+        System.out.println(a4.getClass().getName()); //[Z
+
+        String[] students = new String[3];
+        students[0] = "AA";
+        students[1] = "BB";
+        System.out.println(students[0]);
+        System.out.println(students[1]);
+        System.out.println(students[2]);
     }
 }
